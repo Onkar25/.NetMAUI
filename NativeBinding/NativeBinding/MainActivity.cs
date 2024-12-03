822a0b0aaada00fb430e26be594ac165bb2dbfb2
Onkar Murkar <onkar.murkar@outlook.com>
2024-12-03 10:43:49 +0530

    Added Native Binding Project

---
+++ NativeBinding/NativeBinding/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿namespace NativeBinding;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : Activity
+{
+    NativeLibraryBinding.NativeClass nativeClass = new();
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Set our view from the "main" layout resource
+        SetContentView(Resource.Layout.activity_main);
+
+        
+    }
+
+    [Obsolete]
+    public void ImplementNative()
+    {
+        nativeClass.GetData();
+    }
+}
---
+++ NativeBinding/NativeLibraryBinding/NativeClass.cs
@@ -0,0 +1,35 @@
+﻿using Org.Apache.Commons.Collections4;
+
+namespace NativeLibraryBinding
+{
+    public interface IBoundedMap
+    {
+        public static int BAR = 1;
+    }
+
+    public interface IBoundedMapInvoker
+    {
+        //public IGet.EntrySet();
+    }
+
+    public class NativeClass
+	{
+        [Obsolete]
+        ArrayStack arrayStack = new ArrayStack();
+		public NativeClass()
+		{
+			
+		}
+
+        [Obsolete]
+        public void GetData()
+		{
+			arrayStack.Add(new String("Onkar"));
+            arrayStack.Add(new String("Prasad"));
+            arrayStack.Add(new String("Murkar"));
+
+			Console.WriteLine("Need to print : " + arrayStack);
+        }
+	}
+}
+
