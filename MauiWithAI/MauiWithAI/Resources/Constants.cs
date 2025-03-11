c378b9d18bcda09fc1d6e1778860f77fd34dc5a7
Onkar Murkar <36659095+Onkar25@users.noreply.github.com>
2025-03-12 01:53:16 +0530

    Update Constants.cs

--- MauiWithAI/MauiWithAI/Resources/Constants.cs
+++ MauiWithAI/MauiWithAI/Resources/Constants.cs
@@ -2,6 +2,6 @@ namespace MauiWithAI;
 
 public class Constants
 {
-    public const string OpenAIKey = "";
+    public const string OpenAIKey = "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";
     public const string OpenAIEndpoint = null;
 }
